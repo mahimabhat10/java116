de6d2aa6b89bdd208928fcbd1dd7daba1bcc2c66
Mahima Bhat <mahima.2428cseaiml51@kiet.edu>
2025-09-21 19:04:00 +0530

    Create Person.java

---
+++ src/Unit1/Person.java
@@ -0,0 +1,13 @@
+package Unit_1;
+
+public class Person {
+    public String course;
+
+//    public Unit_1.Person(String address, String course) {
+////        super(address);
+//        this.course = course;
+//    }
+    public void getCourse(){
+        System.out.println("course "+course);
+    }
+}
