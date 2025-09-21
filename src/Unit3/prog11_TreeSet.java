5fea1f79eadb5601d6e9e991289243bc4f800a89
Mahima Bhat <mahima.2428cseaiml51@kiet.edu>
2025-09-21 19:27:01 +0530

    Create prog11_TreeSet.java

---
+++ src/Unit3/prog11_TreeSet.java
@@ -0,0 +1,14 @@
+package Unit_3;
+
+import java.util.TreeSet;
+
+public class prog11_TreeSet {
+    public static void main(String[] args) {
+        TreeSet<String> ts1= new TreeSet<>();
+        ts1.add("crypto");
+        ts1.add("zip");
+        ts1.add("hola");
+        ts1.add("khikhi");
+        System.out.println(ts1);
+    }
+}
