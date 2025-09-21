4c5eb43f9998f05efa77c1782a74606b078d3f06
Mahima Bhat <mahima.2428cseaiml51@kiet.edu>
2025-09-21 19:08:59 +0530

    Create pp4.java

---
+++ src/Unit1/pp4.java
@@ -0,0 +1,13 @@
+package Unit_1;
+
+abstract class pp4 {
+    String name;
+    int roll_no;
+    public pp4(String name, int roll_no) {
+        this.name = name;
+        this.roll_no = roll_no;
+    }
+
+abstract void attend_class();
+abstract void give_exam();
+}
