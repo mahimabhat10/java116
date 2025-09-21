a41322cbe520150d04309eebf522791b1d3f454d
Mahima Bhat <mahima.2428cseaiml51@kiet.edu>
2025-09-21 18:31:58 +0530

    Create Mymain12.java

---
+++ src/Unit1/Mymain12.java
@@ -0,0 +1,13 @@
+package Unit_1;
+
+public class Mymain12 {
+    public static void main(String[] args){
+        prog12 b= new prog12();
+        try {
+            b.withdraw(159534);
+        }
+    catch(prog12_2 a){
+        System.out.println(a.getMessage());
+    }
+    }
+}
