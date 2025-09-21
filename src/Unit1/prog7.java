48c7862429e44ee6b8f323bd3bd2f2b8f37b97ae
Mahima Bhat <mahima.2428cseaiml51@kiet.edu>
2025-09-21 19:17:17 +0530

    Create prog7.java

---
+++ src/Unit1/prog7.java
@@ -0,0 +1,12 @@
+package Unit_1;
+
+public class prog7 {
+    public String address;
+
+    public prog7(String address) {
+        this.address = address;
+    }
+    public void getAddress(){
+        System.out.println("Address is "+address);
+    }
+}
